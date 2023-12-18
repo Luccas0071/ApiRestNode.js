92bc96cef08989f13d710584d82abeda0832587a
luccas0071 <lucasoliveira719@gmail.com>
2023-12-18 19:51:41 -0300

    First commit

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import routes from "./routes/index.js";
+import conectaNaDatabase from "./config/dbconnect.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("Erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco feita com sucesso !");
+});
+
+const app = express();
+routes(app);
+
+
+// app.get("/livros/:id", (req, res) => {
+//     const index = buscaLivros(req.params.id);
+//     res.status(200).json(livros[index]);
+// })
+
+// app.post("/livros", (req, res) => {
+//     livros.push(req.body);
+//     res.status(201).send("Livro cadastrado com sucesso !");
+// });
+
+// app.put("/livros/:id", (req, res) => {
+//     const index = buscaLivros(req.params.id);
+//     livros[index].titulo = req.body.titulo;
+//     res.status(200).json(livros);
+// })
+
+// app.delete("/livros/:id", (req, res) => {
+//     const index = buscaLivros(req.params.id);
+//     livros.splice(index, 1);
+//     res.status(200).send("Livro removido com sucesso");
+// });
+
+
+export default app;
